d9b50ff0cdff0b9563cfb48805006ca960c6b8ce
Siddhesh <siddheshvjadhav20@gmail.com>
2023-10-08 19:26:17 +0530

    Added custome error handler

--- app.js
+++ app.js
@@ -3,21 +3,24 @@ import dotenv from 'dotenv'
 import db from './config/dbConnect.js'
 import bodyParser from 'body-parser'
 import authRoute from './routes/authRoute.js'
+import { errorHandler, notFound } from './middelwares/errorHandler.js'
 
 let app = express()
 dotenv.config()
 let PORT = process.env.PORT || 3113
 
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+app.use('/api/user', authRoute)
+app.use(notFound)
+app.use(errorHandler)
+
+
 
 app.get('/health', (req, res) => {
     res.status(200).send("<h1>Health is OK</h1>")
 })
-
-
-app.use('/api/user', authRoute)
-
 
 app.listen(PORT, (err) => {
     console.log(`Port ${PORT} is running`)
--- controller/userController.js
+++ controller/userController.js
@@ -1,20 +1,15 @@
 import userModel from "../models/userModel";
-
-export const createUser = async (req, res) => {
+import expressAsyncHandler from "express-async-handler";
+export const createUser = expressAsyncHandler(async (req, res) => {
 
     const email = req.body.email;
     const findUser = await userModel.findOne({ email });
 
     if (!findUser) {
         // create new user
-        const newUser =await userModel.create(req.body)
+        const newUser = await userModel.create(req.body)
         res.json(newUser)
     } else {
-        // user exist
-        res.json({
-            msg: "User Already Exists",
-            success: false
-
-        })
+        throw new Error('User Already Exists')
     }
-}
+})
